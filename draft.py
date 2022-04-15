da94c7f756d674771458da182a830eb77e35e37b
soulafaaa <90429421+soulafaaa@users.noreply.github.com>
2022-04-15 14:36:25 -0400

    basic set up
    created a file editor that will hold all our file methods

--- draft.py
+++ draft.py
@@ -1,8 +1,42 @@
-def movie_rec(genre ,genre2, language):
-    """ """
+import csv
+import sys 
 
-def music_rec(genre ,genre2, language):
-    """ """
+
+def recommend_artist(Genres, type):
+    """ This method will compare the argument added to the  list of artist we have and will 
+    recommend a new artist 
+    
+    Args: 
+        Genre (list): valued representing the user favorite music type
+        Type (String): value representing whether the user is looking for an artist orband 
+    Return: 
+        List of recomended artist
+    """
+    
+def recommend_song(Genre,type):
+    """ This method will compare teh arument passed to the existing list of songs we have 
+    and will recommend diffrent songs
+    Args: 
+        Genre (list): values representing the user favorite music type
+        Type (String): value representing whether the user is looking for an artist orband 
+     Return: 
+        List of recomended songs
+    
+    """  
     
 def main():
-    """"""
+    """ the main will collect user input and pass the responses as argument to the other methods.
+    Args:
+        None
+        
+    Return: 
+        String:  with all the  recommnded music based on their 
+    favorite atrist and genre they inputed.
+    """
+    open_file()
+    
+    
+     
+
+if __name__ =="__main__":
+    main()
---
+++ file_editor.py
@@ -0,0 +1,31 @@
+class file_editor():
+    """ this class will hold all helper methods we will need in order to extract data from 
+    the cvs file.
+    
+    Attributes: 
+    
+    
+    """
+    
+    def __init__(file):
+        self.file = file
+        
+        
+    def artist(self):
+        """ this method will extract all the artist and genres from the cvs file and return 
+        them in a list from
+        
+        Args: 
+            None 
+        
+        Returns:
+            returns a list of artist and the genres associated with them. 
+        
+        """
+    
+    def songs(self):
+        """ This method will extract the songs and .......... from the cvs file
+        
+        Return: 
+            Reurns a list of all songs and their details.  
+        """
