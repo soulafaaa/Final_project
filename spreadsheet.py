e3197e94717070763ccf8a0105d843f24b22fb62
soulafaaa <90429421+soulafaaa@users.noreply.github.com>
2022-05-09 09:01:24 -0400

    minor changes

--- spreadsheet.py
+++ spreadsheet.py
@@ -10,43 +10,16 @@ client = gspread.authorize(creds)
 sheet = client.open('Favorite Music Survey (Responses)').sheet1
 pp = pprint.PrettyPrinter()
 #by changing this here we get retrieve different information from the google sheet thagt is attached to out google form.
-all_result  = sheet.get_all_values()  
 
 
-     
-def artist():
-    """ this method will extract all the artist and genres from the cvs file and return 
-        them in a list from
-        
-        Args: 
-            None 
-        
-        Returns:
-            returns a list of artist and the genres associated with them. 
-        
-        """
-    # something like this .... i dont have the tiem to work on this today. can someone finsh this lease
-    ans = sheet.col_values(4)
-    return ans
+
+def getartist(name):
+    # we need to find all the cells that have that artist name in 
+    ans = sheet.findall(name)
+    print(ans)
     
     
-    
-    
-       
-    
-def songs(result):
-        """ This method will extract the songs and .......... from the cvs file
-        
-        Return: 
-            Reurns a list of all songs and their details.  
-        """
-        
+getartist("beyonce")
 
 
-lol = artist()
-print(lol)
 
-
-        
-    
-    
